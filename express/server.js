bb4b039e0faa9994d42ffe6679723a06d6b3b1e1
Rodrigo <roh.amelo@gmail.com>
2023-01-19 15:15:33 -0300

    Super fix

--- express/server.js
+++ express/server.js
@@ -1,16 +1,20 @@
 'use strict';
-const Element = require("./functions/Element");
+const Element = require("./Element");
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 const STORE = {};
-
 const app = express();
+
 
 app.use(cors());
 
-app.get("/get/:key", function(req, res) {
+const router = express.Router();
+router.get("/get/:key", function(req, res) {
 	const { key } = req.params;
 	const element = STORE[key];
 
@@ -19,7 +23,7 @@ app.get("/get/:key", function(req, res) {
 		: res.status(404).json({ error: "The key wasn't found" });
 });
 
-app.get("/set/:key/:value", function(req, res) {
+router.get("/set/:key/:value", function(req, res) {
 	const { key, value } = req.params;
 
 	if (STORE[key])
@@ -34,6 +38,17 @@ setInterval(() => {
 	for (let key in STORE) if (STORE[key].hasExpired) delete STORE[key];
 }, 1000);
 
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
 app.listen(PORT, function() {
 	console.log(`Listening on port ${PORT}!`);
 });
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
+
+
